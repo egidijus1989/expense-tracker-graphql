279564cb35f8a87c42176528565ad0ec847d8eb9
egidijus <145454567+egidijus1989@users.noreply.github.com>
2024-05-27 19:17:13 +0300

    first commit

---
+++ backend/index.js
@@ -0,0 +1,76 @@
+import express from "express";
+import http from "http";
+import cors from "cors";
+import dotenv from "dotenv";
+import path, { resolve } from "path";
+import passport from "passport";
+import session from "express-session";
+import connectMongo from "connect-mongodb-session";
+
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { GraphQLLocalStrategy, buildContext } from "graphql-passport";
+
+import mergedResolvers from "./resolvers/index.js";
+import mergedTypeDefs from "./typeDefs/index.js";
+import { connectDB } from "./db/connectDB.js";
+import { configurePassport } from "./passport/passport.config.js";
+
+dotenv.config();
+configurePassport();
+
+const app = express();
+const httpServer = http.createServer(app);
+
+const MongoDBStore = connectMongo(session);
+const store = new MongoDBStore({
+  uri: process.env.MONGO_URI,
+  collection: "sessions",
+});
+
+store.on("error", (err) => console.log(err));
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false, //specifies wheter to save the session to the store on every request
+    saveUninitialized: false, // specifies wheter to save uninitialized sessions
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7, //1week
+      httpOnly: true,
+    },
+    store: store,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+const server = new ApolloServer({
+  typeDefs: mergedTypeDefs,
+  resolvers: mergedResolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+});
+
+await server.start();
+
+app.use(
+  "/graphql",
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  }),
+  express.json(),
+  expressMiddleware(server, {
+    context: async ({ req, res }) => buildContext({ req, res }),
+  })
+);
+
+await new Promise((resolve) =>
+  httpServer.listen({ port: process.env.PORT }, resolve)
+);
+await connectDB();
+
+console.log(`🚀 Server ready at http://localhost:${process.env.PORT}/graphql`);
---
+++ frontend/src/main.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import GridBackground from "./components/ui/GridBackground.jsx";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:8888/graphql", // the URL of our GraphQL server.
+  cache: new InMemoryCache(), // Apollo Client uses to cache query results after fetching them.
+  credentials: "include", // send cookes to apollo server
+});
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <GridBackground>
+        <ApolloProvider client={client}>
+          <App />
+        </ApolloProvider>
+      </GridBackground>
+    </BrowserRouter>
+  </React.StrictMode>
+);
